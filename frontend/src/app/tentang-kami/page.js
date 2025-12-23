1096c65f91784fdc177f368554b723bf4d399fa2
naliya643 <hilaliyah643@gmail.com>
2025-12-24 02:05:22 +0700

    rawr

--- frontend/src/app/tentang-kami/page.js
+++ frontend/src/app/tentang-kami/page.js
@@ -87,7 +87,7 @@ export default function TentangKami() {
         >
           {/* FOTO BULET */}
           <img
-            src="/gue.jpeg"
+            src="/guehhh.jpeg"
             alt="Foto Pengembang"
             className="w-35 h-35 rounded-full object-cover border-2 shadow-sm"
             style={{ borderColor: COLORS.title }}
